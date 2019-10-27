5f5a8f65989a100b42f88fd57dc373077bc8bf12
Arve Seljebu <arve.seljebu@gmail.com>
2019-10-27 21:11:31 +0100

    visning av variabler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 function App() {
+  let t = "tekst"
+  let n = 0
+  let o = {
+    k: 99
+  }
+  let ok = o.k
+  let { k } = o; // destructuring
+  let a = [1, 2, 3];
+  let [første, andre, tredje] = a;
+
   return (
     <div>
       <p>hallo</p>
+      <p>t: {t}</p>
+      <p>n: {n}</p>
+      <p>o.k: {o.k}</p>
+      <p>o.k: {ok}</p>
+      <p>o.k: {k}</p>
+      <p>a: {a}</p>
+      <ul>
+        {a.map((verdi, indeks) =>
+          <li>{indeks}: {verdi}</li>
+        )}
+      </ul>
+      <p>a: {første} + {andre} + {tredje}</p>
     </div>
   );
 }
