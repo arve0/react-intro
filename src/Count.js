eebbb348da4c3f5d05514d6eeb295b18244090eb
Arve Seljebu <arve.seljebu@gmail.com>
2019-10-28 20:56:56 +0100

    løsning

---
+++ src/Count.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Count(props) {
+    let n = props.n;
+
+    return (
+        <p>
+            hallo {n}
+            <button onClick={props.inc}>+</button>
+        </p>
+    )
+}
+
+export default Count
