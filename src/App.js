ebbfccaa4de93db8ced8772c858cd7d5bb4526d3
Arve Seljebu <arve.seljebu@gmail.com>
2019-10-28 19:52:12 +0100

    mange tellere

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  let [counts, setCounts] = useState([])
+
+  function add() {
+    setCounts([...counts, 0])
+  }
+
+  function inc(index) {
+    counts[index] += 1;
+    setCounts([...counts]);
+  }
+
   return (
     <div>
       <p>hallo</p>
+      <button onClick={add}>add counter</button>
+      {counts.map((value, index) =>
+        <Count value={value} inc={() => inc(index)} />
+      )}
     </div>
   );
+}
+
+function Count({value, inc}) {
+  return (
+    <p>
+      {value}
+      <button onClick={inc}>+</button>
+    </p>
+  )
 }
 
 export default App;
